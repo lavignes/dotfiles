f708fa8c21362a227a532cc2446b3a4196c18a20
Scott LaVigne <lavignes@amazon.com>
2023-10-31 10:55:16 -0700

    fix sync

--- sync.sh
+++ sync.sh
@@ -197,7 +197,7 @@ sync_gui() {
         apt_install "alacritty"
         apt_install "neovim"
 
-        /usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2023.02.18_all.deb keyring.deb SHA256:a511ac5f10cd811f8a4ca44d665f2fa1add7a9f09bef238cdfad8461f5239cc4 "$workdir/keyring.deb"
+        /usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2023.02.18_all.deb "$workdir/keyring.deb" SHA256:a511ac5f10cd811f8a4ca44d665f2fa1add7a9f09bef238cdfad8461f5239cc4
         sudo apt install "$workdir/keyring.deb"
         echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" | sudo tee /etc/apt/sources.list.d/sur5r-i3.list
         sudo apt update
