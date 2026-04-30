47b55713879ed133c8e72d5ecb0d1f81bc52209c
Scott LaVigne <lavignes@amazon.com>
2026-04-30 02:45:38 -0700

    fix(vim): Use CFLAGS env var for -mcmodel=medium
    CMAKE_EXTRA_FLAGS only affects the main build, not the nested .deps
    build where tree-sitter parsers are compiled. Set CFLAGS as an env
    var so it propagates to all sub-builds.

--- sync.sh
+++ sync.sh
@@ -278,9 +278,9 @@ sync_vim() {
                 "https://github.com/neovim/neovim.git" "$workdir/neovim"
             cd "$workdir/neovim"
             if [ "$os" = "yum" ]; then
-                make -j CC="$HOME/.local/bin/gcc" CXX="$HOME/.local/bin/g++" \
-                    CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX="$HOME/.local" \
-                    CMAKE_EXTRA_FLAGS="-DCMAKE_C_FLAGS='-mcmodel=medium'"
+                CFLAGS="-mcmodel=medium" make -j \
+                    CC="$HOME/.local/bin/gcc" CXX="$HOME/.local/bin/g++" \
+                    CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX="$HOME/.local"
             else
                 make -j CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX="$HOME/.local"
             fi
